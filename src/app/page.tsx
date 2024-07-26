cb89921267444e5f65d2dd47292de2f788b23eac
analog-sombra <analogsombra@gmail.com>
2024-07-26 17:25:23 +0530

    fix: heading added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,28 +18,27 @@ export default function Home() {
 
   return (
     <main className="bg-[#e1e9eb] p-12">
-      <div>
-        <div className="flex gap-4 items-center justify-evenly">
-          <div className="bg-[#2e8ee2] px-2 py-1 rounded shadow text-white">
-            Server DateTime:
-            {serverTime == null ? "" : formatDateTime(serverTime)}
-          </div>
-          <div className="bg-[#2e8ee2] px-2 py-1 rounded shadow text-white">
-            Datebase DateTime:
-            {databaseTime == null ? "" : formatDateTime(databaseTime)}
-          </div>
-          <div className="bg-[#2e8ee2] px-2 py-1 rounded shadow text-white">
-            Local DateTime: {localTime == null ? "" : formatDateTime(localTime)}
-          </div>
+      <h1 className="text-center text-lg text-[#2e8ee2]">This is just a heading</h1>
+      <div className="flex gap-4 items-center justify-evenly">
+        <div className="bg-[#2e8ee2] px-2 py-1 rounded shadow text-white">
+          Server DateTime:
+          {serverTime == null ? "" : formatDateTime(serverTime)}
         </div>
-        <div className="flex items-center justify-center mt-4">
-          <button
-            onClick={settime}
-            className="bg-blue-500 bg-gradient-to-t from-[#2377d4] to-[#96d0f5] text-white  px-2 py-1 text-lg rounded-md"
-          >
-            Get Time
-          </button>
+        <div className="bg-[#2e8ee2] px-2 py-1 rounded shadow text-white">
+          Datebase DateTime:
+          {databaseTime == null ? "" : formatDateTime(databaseTime)}
         </div>
+        <div className="bg-[#2e8ee2] px-2 py-1 rounded shadow text-white">
+          Local DateTime: {localTime == null ? "" : formatDateTime(localTime)}
+        </div>
+      </div>
+      <div className="flex items-center justify-center mt-4">
+        <button
+          onClick={settime}
+          className="bg-blue-500 bg-gradient-to-t from-[#2377d4] to-[#96d0f5] text-white  px-2 py-1 text-lg rounded-md"
+        >
+          Get Time
+        </button>
       </div>
     </main>
   );
